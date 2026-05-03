679eae984f8981cb5a63a8aa5c41a214a65598b8
ashish67915-prog <ashish67915@gmail.com>
2026-05-03 09:44:42 +0530

    STL (set)

---
+++ stlSet.cpp
@@ -0,0 +1,18 @@
+//how to use Set 
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    set<int>s;
+    s.insert(10);
+    s.insert(20);
+    s.insert(30);
+    s.insert(40);
+    s.insert(50);
+    for(auto it=s.begin() ;it!=s.end() ;it++)
+    {
+        cout<<*it<<endl;
+    }
+}
